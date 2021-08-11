49941165d7a6645eceaf3353f35c4ce2f5f29423
Rees <40927062+rees047@users.noreply.github.com>
2021-08-11 16:56:30 -0400

    fix erros
    changed class to classname

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -11,14 +11,14 @@ function WelcomeScreen(props) {
         <Container className="WelcomeScreen">
             <Row className="justify-content-md-center">
                 <Col lg={10} className="WelcomeScreen-inner-wrapper">
-                    <div class="d-flex justify-content-center align-items-center h-500">
+                    <div className="d-flex justify-content-center align-items-center h-500">
                         <div>             
                             <h1>Welcome to the Meet app</h1>
                             <h4>Log in to see upcoming events around the world for full-stack developers</h4>
                             <div className="button_cont" align="center">
-                                <div class="google-btn">
-                                    <div class="google-icon-wrapper">
-                                        <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="Google sign-in" />
+                                <div className="google-btn">
+                                    <div className="google-icon-wrapper">
+                                        <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="Google sign-in" />
                                     </div>
                                     <button onClick={() => { props.getAccessToken() }} rel="nofollow noopener" class="btn-text">
                                         <b>Sign in with google</b>
